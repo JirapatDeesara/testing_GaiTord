7c34fc78795d096c2601c69e945d6a380ee3bccd
Jirapat <jirapat.dees@bumail.net>
2024-12-02 13:46:08 +0700

    Update

--- Assets/Script/HealthManager.cs
+++ Assets/Script/HealthManager.cs
@@ -7,18 +7,6 @@ public class HealthManager : MonoBehaviour
     public int IncreaseHealth(int currentHealth, int increaseAmount, int maxHealth)
     { 
      int newHealth = currentHealth + increaseAmount;
-        return ClampHealth(currentHealth, newHealth);
+       return Mathf.Clamp(currentHealth, 0, maxHealth);
     }// End IncreaseHealth method
-
-    public int ClampHealth(int currentHealth, int maxHealth)
-    {
-        if (currentHealth > maxHealth)
-        { 
-          return maxHealth;
-        }
-        else
-        {
-            return currentHealth;
-        }
-    }// End ClampHealth method
 }
